c9fb8f18aa7facb309a85b159e7cf1719365bc3b
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-09-11 10:43:37 -0400

    Update Footer.jsx

--- lib/components/Footer.jsx
+++ lib/components/Footer.jsx
@@ -39,7 +39,7 @@ export const Footer = () => {
             <a
               title='auditAndSecurityInfo'
               className='trans mr-4'
-              href='https://docs.mune.finance'
+              href='https://docs.pooltogether.com/security/audits-and-testing'
             >
               audits
             </a>
